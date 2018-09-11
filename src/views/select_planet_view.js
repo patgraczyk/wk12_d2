9e79a3ce984f1331a7f7e85ec40edee9e051f346
Vicky Jackson <vicky@digitalstuff.net>
2018-09-11 15:43:01 +0100

    Added a bit of styling

--- src/views/select_planet_view.js
+++ src/views/select_planet_view.js
@@ -8,14 +8,22 @@ SelectPlanetView.prototype.bindEvents = function(){
   const menu = document.querySelectorAll('.planet-menu-item');
 
   menu.forEach(menuItem => {
+
+    console.log(menuItem);
     menuItem.addEventListener('click', (event) => {
       const selectedPlanet = event.target.id;
       //console.log(selectedPlanet);
       PubSub.publish('SelectPlanetView:change', selectedPlanet);
     })
+
   });
 };
 
 module.exports = SelectPlanetView;
+
+// menuItem.addEventListener('mouseover', (event) => {
+//   event.target.style.color = "blue";
+//   //menuItem.classList.add('hover');
+// });
 
 
